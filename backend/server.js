14644bd080c8aa4031ba20d9ee1e34ad86059c02
student Preetivaza <preetivaza@gmail.com>
2026-04-17 08:08:43 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://your-frontend.vercel.app",
+    origin: "https://inventorymanagementsystem-1-qe5j.onrender.com",
     credentials: true,
   }),
 );
